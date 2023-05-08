93352cdb230ed74775bb0bdea91270d15c613ea9
suchandra <129024914+suchandra12@users.noreply.github.com>
2023-05-08 16:23:56 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,11 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("mannasu marchukundam"); 
-        System.out.println("mannasu marchukundam"); 
-        System.out.println("mannasu marchukundam"); 
-        System.out.println("reddy"); 
-        System.out.println("suhansh"); 
+        
         System.out.println("guntur"); 
     }
 }
